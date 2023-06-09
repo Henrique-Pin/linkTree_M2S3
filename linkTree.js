b3c1a610c45c2c5ff31047771b706dfdf785a5dc
Henrique-Pin <134744208+Henrique-Pin@users.noreply.github.com>
2023-06-09 02:05:43 +0000

    Concluida

---
+++ linkTree.js
@@ -0,0 +1,53 @@
+var item1 = document.getElementById('butao1')
+item1.addEventListener('mouseover', escureçer1)
+item1.addEventListener('mouseout', normalizar1)
+function escureçer1() {
+    item1.style.background = '#8b8b8b'
+}
+function normalizar1() {
+    item1.style.background = '#e9e9e9'
+}
+
+
+var item2 = document.getElementById('butao2')
+item2.addEventListener('mouseover', escureçer2)
+item2.addEventListener('mouseout', normalizar2)
+function escureçer2() {
+    item2.style.background = '#8b8b8b'
+}
+function normalizar2() {
+    item2.style.background = '#e9e9e9'
+}
+
+
+var item3 = document.getElementById('butao3')
+item3.addEventListener('mouseover', escureçer3)
+item3.addEventListener('mouseout', normalizar3)
+function escureçer3() {
+    item3.style.background = '#8b8b8b'
+}
+function normalizar3() {
+    item3.style.background = '#e9e9e9'
+}
+
+
+var item4 = document.getElementById('butao4')
+item4.addEventListener('mouseover', escureçer4)
+item4.addEventListener('mouseout', normalizar4)
+function escureçer4() {
+    item4.style.background = '#8b8b8b'
+}
+function normalizar4() {
+    item4.style.background = '#e9e9e9'
+}
+
+
+var item5 = document.getElementById('butao5')
+item5.addEventListener('mouseover', escureçer5)
+item5.addEventListener('mouseout', normalizar5)
+function escureçer5() {
+    item5.style.background = '#8b8b8b'
+}
+function normalizar5() {
+    item5.style.background = '#e9e9e9'
+}
